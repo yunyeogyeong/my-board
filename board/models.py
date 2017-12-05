c274f7db13f1104a77595e81af9a21b3eacf90e3
shin <actionshin@gmail.com>
2017-12-05 16:18:57 +0900

    게시판 시작

---
+++ board/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    modified_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+# class Comment(models.Model):
+#     post = models.ForeignKey('board.Post', related_name='comments')
+#     author = models.CharField(max_length=200)
+#     text = models.TextField()
+#     created_date = models.DateTimeField(default=timezone.now)
+#     approved_comment = models.BooleanField(default=False)
+
+#     def approve(self):
+#         self.approved_comment = True;
+#         self.save()
+    
+#     def __str__(self):
+#         return self.text
