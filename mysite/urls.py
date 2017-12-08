329ec063d26ad76970ed1a8f1ba1c163162e5ea3
shin <actionshin@gmail.com>
2017-12-08 14:45:43 +0900

    bootstrap 적용

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from mysite.views import HomeView
+from mysite.views import UserRegisterView, UserRegisterDoneView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^blog/', include('board.urls')),
+    url(r'^accounts/register/$', UserRegisterView.as_view(), name='register'),
+    url(r'^accounts/register/done/$', UserRegisterDoneView.as_view(), name='register_done'),
+    url(r'^board/', include('board.urls', namespace='board')),
 ]
